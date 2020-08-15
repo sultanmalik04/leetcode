e53c66a6a785ae4eb8f95d0a9b37525e4c864db3
sultan malik <48114122+sultanmalik04@users.noreply.github.com>
2020-08-15 15:06:00 +0530

    1st

--- TwoSum.py
+++ TwoSum.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        dic = dict()
-        for i in range(len(nums)):
-            dic[nums[i]] = i
+        dic = dict((nums[i],i) for i in range(len(nums)))
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in dic.keys() and dic[complement] != i:
                 return [i, dic[complement]]
-
 
 nums = list(map(int, input().split()))
 target = int(input())
