61bf9af6ca4a5a5ea901e4af5095d62fd78cc3ee
sultan malik <48114122+sultanmalik04@users.noreply.github.com>
2020-08-15 14:35:43 +0530

    1st

---
+++ TwoSum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        dic = dict()
+        for i in range(len(nums)):
+            dic[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dic.keys() and dic[complement] != i:
+                return [i, dic[complement]]
+
+
+nums = list(map(int, input().split()))
+target = int(input())
+result = Solution()
+print(result.twoSum(nums,target))
